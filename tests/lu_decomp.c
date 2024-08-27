8001e100aa6fb7e8140932881348a50b5d5a9752
michbogos <bogostamicheal@gmail.com>
2024-08-27 20:10:06 +0200

    Add test case for lu decomp

--- tests/lu_decomp.c
+++ tests/lu_decomp.c
@@ -2,27 +2,27 @@
 #include"../matrix.h"
 
 int main(){
-    float A[9] = {1, 3, 1, 9, 1, 1, -1, 1, 3};
-    float alpha[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    float beta[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    lu_decomp(A, alpha, beta, 3, 4);
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%f ", A[i*4+j]);
+    float A[4] = {4, 3, 6, 3};
+    float alpha[4] = {0, 0, 0, 0};
+    float beta[4] = {0, 0, 0, 0};
+    lu_decomp(A, alpha, beta, 2, 2);
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%f ", A[i*2+j]);
         }
         printf("\n");
     }
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%f ", alpha[i*4+j]);
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%f ", alpha[i*2+j]);
         }
         printf("\n");
     }
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%f ", beta[i*4+j]);
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%f ", beta[i*2+j]);
         }
         printf("\n");
     }
