56e2f41cee6a7eb06fce800a747b3dd8092e925d
michbogos <bogostamicheal@gmail.com>
2024-09-06 21:16:38 +0200

    Add polynomial evaluation

---
+++ polynomial.h
@@ -0,0 +1,5 @@
+float eval_poly(float* coeffs, int num_coeffs, float x){
+    float res = coeffs[num_coeffs-1];
+    for(int i = num_coeffs-2 ; i > 0; i--) res = res*x+coeffs[i];
+    return res;
+}
