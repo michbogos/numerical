310c703c673a8bed2e2fc7c57ac0969d41fe459a
michbogos <bogostamicheal@gmail.com>
2024-09-07 22:48:56 +0200

    Add uint random number generation

--- random.h
+++ random.h
@@ -1,1 +1,12 @@
-float eval
+struct rand_state{
+    unsigned long long state;
+    unsigned long long inc;
+};
+
+unsigned int rand_uint(struct rand_state* rng){
+    unsigned long long oldstate = rng->state;
+    rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
+    unsigned int xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    unsigned int rot = oldstate >> 59u;
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
