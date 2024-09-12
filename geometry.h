1f6fb1f50c5f9f697cb309b125bdb9bab3102f4a
michbogos <bogostamicheal@gmail.com>
2024-09-12 20:15:43 +0200

    Renae shape.h and add triangle

--- geometry.h
+++ geometry.h
@@ -1,3 +1,17 @@
+#include<vec.h>
+
+struct triangle_2d{
+    struct vec2 a;
+    struct vec2 b;
+    struct vec2 c;
+};
+
+struct triangle_3d{
+    struct vec3 a;
+    struct vec3 b;
+    struct vec3 c;
+};
+
 float shape_area(float* xy_coords, int n_points){
     float res = 0;
     for(int i = 1; i < n_points-1; i++){
